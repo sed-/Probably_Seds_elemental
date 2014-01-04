bb43dfbfd8cc67a6db880ca8ab2b8fa6eb4c84b2
sed- <chew.head1@gmail.com>
2014-01-04 16:16:13 -0700

    ver 5

--- rotation.lua
+++ rotation.lua
@@ -5,7 +5,7 @@ ProbablyEngine.rotation.register_custom(262, "Seds elemental", {
 
 --Self def//heal
 {"108270", "player.health < 65"},
-{"8004", "player.health < 60"},
+--{"8004", "player.health < 60"},
 
 --Aoe rotation
 {"421", "modifier.lalt"},
